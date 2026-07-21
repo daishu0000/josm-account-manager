7d834ad29aa63f1b8bfc27b14d700f8c009df60f
daishu0000 <daishu10000@gmail.com>
2026-07-21 15:46:12 +0800

    release 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.api.tasks.JavaExec
 import org.openstreetmap.josm.gradle.plugin.task.github.PublishToGithubReleaseTask
 import java.util.Properties
 
-version = "0.1.0"
+version = "0.1.1"
 
 val releaseJarName = "account_manager.jar"
 val releaseJarPath = layout.buildDirectory.file("dist/$releaseJarName")
